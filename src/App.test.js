f3fe9478efcefa6030605f1aa18d43c26a526aa4
sony <sonyk@softpauer.com>
2022-12-11 12:15:17 +0000

    add better variable names

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-
-
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 test('smoke test', () => { 
   expect(2+2).toBe(4);
+});
 
-test('renders learn react link', () => {
+test('renders H1 with welcome to dog lovers', () => {
   render(<App />);
-  const linkElement = screen.getByText(/this is the page for dog lovers/i);
-  expect(linkElement).toBeInTheDocument();
-
+  const headerWelcome = screen.getByText(/this is the page for dog lovers/i);
+  expect(headerWelcome).toBeInTheDocument();
 });
