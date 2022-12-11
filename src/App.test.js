8714ece7b86b384c23460e2e4c4d4d8ba0288ebf
sony <sonyk@softpauer.com>
2022-12-11 11:32:10 +0000

    correcting the landing page text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/This is the page for dog lovers/i);
+  const linkElement = screen.getByText(/this will be the landing page/i);
   expect(linkElement).toBeInTheDocument();
 });
