c9e565d9d4cfed61b00ebba9ee52add4e0555847
sony <sonyk@softpauer.com>
2022-12-15 18:25:37 +0000

    Added integrate-appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './images/logopic.svg';
+import { useState } from 'react';
+import {add2} from './utils/utils';
 import './App.css';
 
 function App() {
+  let [userNumber,setUserNumber]=useState("");
+ 
+  const acceptInPut=(element)=>{
+    setUserNumber(element.target.value);
+  }
+  console.log(userNumber)
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +17,13 @@ function App() {
         <h1>
           This is the page for dog lovers
         </h1>
-        <h2 className="greeting-h2">Welcome</h2>
-        <h3>to my page</h3>
+        <h2 className="greeting-h2">Enter a number to square</h2>
+        <input
+          type="number" 
+          id="input-box"
+          onChange={acceptInPut}
+        />
+        <p>result {add2(userNumber)}</p>
       </header>
     </div>
   );
